ccdecb753a811664f9e2d938cd0ad6cd39f14a2f
RobertaCasu <robertacasu@ymail.com>
2018-11-30 15:08:59 +0100

    tentativoindex

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const PORT = 3000 || process.env.port;
 
 require('./config/db.js');
 require('./routes/routes.js')(app, express)
+ 
 
 app.listen(PORT, () => {
     console.log('http://localhost:' + PORT)
