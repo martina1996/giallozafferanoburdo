68b6237a9e4429a714b2c810f768001420231bbb
formazione1 <formazione1@sidasrl.int>
2018-11-30 15:11:23 +0100

    revert

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 3000 || process.env.port;
+
+require('./config/db.js');
+require('./routes/routes.js')(app, express)
+ 
+
+app.listen(PORT, () => {
+    console.log('http://localhost:' + PORT)
+})
