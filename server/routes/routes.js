1705477ee07a24bc60756423c11ee8f3bfc14e44
RobertaCasu <robertacasu@ymail.com>
2018-11-30 16:52:36 +0100

    jasonmmerda

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,6 +1,8 @@
 module.exports = function(app, express){
-   
-
+    var bodyParser = require('body-parser');
+    app.use(bodyParser.json());
+    var path = require('path');
+    
     //  app.use('/ingredienti', require('../resource/ingredienti/'));
      app.use('/ricette', require('../resource/ricette/'));
 }
