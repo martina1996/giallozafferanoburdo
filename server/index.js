550f94c8d893a57e9453256caf52d32d814d3d0f
formazione1 <formazione1@sidasrl.int>
2018-12-01 09:36:47 +0100

    fix

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.port  || 21460 ;
+const PORT = process.env.PORT  || 3000 ;
 
 require('./config/db.js');
 require('./routes/routes.js')(app, express)
