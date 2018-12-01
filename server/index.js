fcd1c60362929e6f8366fc45f9ebc345d319cb97
formazione1 <formazione1@sidasrl.int>
2018-12-01 09:23:38 +0100

    modifica porta

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3000 || process.env.port;
+const PORT = process.env.port  || 3000 ;
 
 require('./config/db.js');
 require('./routes/routes.js')(app, express)
