5425bd98b9680bd9375f7b1f90ed6daa355596c0
formazione1 <formazione1@sidasrl.int>
2018-12-01 09:35:03 +0100

    prova porta

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.port  || 3000 ;
+const PORT = process.env.port  || 21460 ;
 
 require('./config/db.js');
 require('./routes/routes.js')(app, express)
