db000276886de9bc5a3adc2e97066f7e55fc8d2c
formazione1 <formazione1@sidasrl.int>
2018-12-05 15:37:45 +0100

    navbar

--- public/app/filtri/filter.js
+++ public/app/filtri/filter.js
@@ -4,6 +4,14 @@ angular.module('app')
             return input.toUpperCase()
         }})
 
+    // .filter('filtraPortata', function(){
+    //     return function(input, portata){
+    //         if (!portata) return input
+    //         return input.filter(el => {
+    //             return el.portata.includes(portata)
+    //         })
+    //     }})
+
     // .filter('capitali', function () {
     //             return function (input) {
     //                 return input.toUpperCase()
