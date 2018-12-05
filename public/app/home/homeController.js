5a076fd8b73d1dd98f8e535a030a6b166b5dbbf8
RobertaCasu <robertacasu@ymail.com>
2018-12-05 09:47:24 +0100

    ?

--- public/app/home/homeController.js
+++ public/app/home/homeController.js
@@ -5,6 +5,29 @@ angular.module('app')
         $scope.ricette = ricette;
         
 
+        this.slides = [
+            {
+                title: "1 title",
+                image: 'http://lorempixel.com/560/400/sports/1',
+            },
+            {
+                title: "2 title",
+                image: 'http://lorempixel.com/560/400/sports/2',
+            },
+            {
+                title: "3 title",
+                image: 'http://lorempixel.com/560/400/sports/3',
+            },
+            {
+                title: "4 title",
+                image: 'http://lorempixel.com/560/400/sports/4',
+            },
+            {
+                title: "5 title",
+                image: 'http://lorempixel.com/560/400/sports/5',
+            },
+        ];
+
 
 
 
